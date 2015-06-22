d3025c4b5334ede9fd50960ccdc7327c6c49b47c
Rossano <rossanorb@gmail.com>
2015-06-22 18:54:13 -0300

    alteração do host

--- DELETE.php
+++ DELETE.php
@@ -1,7 +1,8 @@
 <?php
 $token = 'a5w878a8awe8ae8aw7e8a';
 
-$ch = curl_init("http://localhost/api/users/16");
+//$ch = curl_init("http://localhost/api/users/16");
+$ch = curl_init("http://slim/api/users/16");
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
 curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
